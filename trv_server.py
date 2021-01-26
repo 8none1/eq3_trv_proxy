94fc661ddf0c7c6152ac631eef63d7831e659d4c
Will Cooke <will@whizzy.org>
2021-01-26 20:30:01 +0000

    None

--- trv_server.py
+++ trv_server.py
@@ -23,7 +23,7 @@ trv_lookup = {
 "00:1A:22:0D:A3:6B" : "Study"
 }
 
-trv_lookup = {"00:1A:22:0D:A3:6B" : "Study"}
+trv_lookup = {"00:1A:22:0C:2C:BB" : "Sam"}
 
 remote_workers = ["192.168.42.100"]
 
